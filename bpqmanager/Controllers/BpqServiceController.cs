70ff789de6db09d2b804aa01007ac553b806aeda
Tom <tom@m0lte.uk>
2023-05-25 22:45:31 +0100

    Create BpqServiceController.cs

---
+++ bpqmanager/Controllers/BpqServiceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bpqmanager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BpqServiceController : ControllerBase
+    {
+        // install, uninstall, stop, start, update, configure
+    }
+}
