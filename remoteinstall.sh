9c573902f1bafc35e1b2ab6485c15de4945f7ce0
Tom M0LTE <37816024+M0LTE@users.noreply.github.com>
2023-06-12 18:47:13 +0100

    Update remoteinstall.sh

--- remoteinstall.sh
+++ remoteinstall.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
-
 wget -q https://nightly.link/M0LTE/bpqmanager/workflows/dotnet/master/bpqmanager.zip
+sudo systemctl stop bpqmanager
+sudo rm -rf /opt/bpqmanager
 sudo mkdir /opt/bpqmanager
-sudo unzip bpqmanager.zip -d /opt/bpqmanager/
+sudo unzip -q bpqmanager.zip -d /opt/bpqmanager/
 rm bpqmanager.zip
 sudo chmod +x /opt/bpqmanager/bpqmanager
 sudo sh -c 'echo "[Unit]
