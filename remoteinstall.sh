05763ea554bba7b45037768927255ddd53fcb41d
Tom M0LTE <37816024+M0LTE@users.noreply.github.com>
2023-06-12 21:54:14 +0100

    Update remoteinstall.sh

--- remoteinstall.sh
+++ remoteinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 wget -q https://nightly.link/M0LTE/bpqmanager/workflows/dotnet/master/bpqmanager.zip
-sudo systemctl stop bpqmanager
+sudo systemctl stop bpqmanager > /dev/null
 sudo rm -rf /opt/bpqmanager
 sudo mkdir /opt/bpqmanager
 sudo unzip -q bpqmanager.zip -d /opt/bpqmanager/
@@ -14,5 +14,6 @@ WorkingDirectory=/opt/bpqmanager
 Restart=always
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/bpqmanager.service'
-sudo systemctl enable bpqmanager
+sudo systemctl enable bpqmanager > /dev/null
 sudo systemctl start bpqmanager
+echo All done, please visit http://`hostname`:5000 from another PC on your LAN
