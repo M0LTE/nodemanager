719a098956bc3234ce0b1c47e21888e82dc26f6b
Tom M0LTE <37816024+M0LTE@users.noreply.github.com>
2023-06-12 18:21:32 +0100

    Update remoteinstall.sh

--- remoteinstall.sh
+++ remoteinstall.sh
@@ -1,3 +1,17 @@
-echo hello world
-sudo tail /var/log/syslog
-echo line 3
+#!/bin/bash -e
+
+wget -q https://nightly.link/M0LTE/bpqmanager/workflows/dotnet/master/bpqmanager.zip
+sudo mkdir /opt/bpqmanager
+sudo unzip bpqmanager.zip -d /opt/bpqmanager/
+rm bpqmanager.zip
+sudo chmod +x /opt/bpqmanager/bpqmanager
+sudo sh -c 'echo "[Unit]
+After=network.target
+[Service]
+ExecStart=/opt/bpqmanager/bpqmanager --urls http://*:5000
+WorkingDirectory=/opt/bpqmanager
+Restart=always
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/bpqmanager.service'
+sudo systemctl enable bpqmanager
+sudo systemctl start bpqmanager
