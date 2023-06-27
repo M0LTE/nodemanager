5f69eed4806c25983b5ddc5245f6abed1d1de523
Tom <tom@m0lte.uk>
2023-06-27 18:13:06 +0100

    WIP

---
+++ bpqmanager/Pages/BpqGuidedSetup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace bpqmanager.Pages
+{
+    public class BpqGuidedSetupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
